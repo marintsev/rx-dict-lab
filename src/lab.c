f6d677925a2df6c59301c0755305a8c17ed59979
Myron Marintsev <marintsev@gmail.com>
2016-02-08 03:00:53 +0300

    Три случая удаления.

--- src/lab.c
+++ src/lab.c
@@ -41,6 +41,20 @@ struct node_t * btree_create() {
 	return new;
 }
 
+void btree_node_free(struct node_t * x) {
+	assert(x->pointers != NULL);
+	assert(x->keys != NULL);
+	assert(x->is_leaf || x->n == 0);
+
+	free(x->pointers);
+	int i;
+	for (i = 0; i < x->n; i++) {
+		free(x->keys[i]);
+	}
+	x->n = 0;
+	free(x->keys);
+}
+
 // x -- ссылка на родительский узел
 // i -- номер потомка, который заполнен и который надо разделить
 void btree_split_child(struct node_t * x, int i) {
@@ -76,7 +90,7 @@ void btree_split_child(struct node_t * x, int i) {
 	for (j = x->n - 1; j >= i - 1; j--)
 		x->keys[j + 1] = x->keys[j]; // !!!
 	// устанавливаем между k_i и k_{i+1} бывший центральный ключ в y
-	x->keys[i] = y->keys[MIN_CHILDREN-1]; // !!!
+	x->keys[i] = y->keys[MIN_CHILDREN - 1]; // !!!
 	// в родительском теперь на одну запись больше
 	x->n++;
 }
@@ -85,10 +99,16 @@ int btree_node_is_full(struct node_t * x) {
 	return (x->n == 2 * MIN_CHILDREN - 1);
 }
 
-void btree_node_copy_key( struct node_t * x, int i, char * k )
-{
-	x->keys[i] = malloc( strlen( k ) + 1 );
-	strcpy( x->keys[i], k );
+void btree_node_load_key(struct node_t * x, int i, char * k) {
+	x->keys[i] = malloc(strlen(k) + 1);
+	strcpy(x->keys[i], k);
+}
+
+char * btree_node_save_key(struct node_t * x, int i) {
+	char * key = x->keys[i];
+	char * result = malloc(strlen(key) + 1);
+	strcpy(result, key);
+	return result;
 }
 
 void btree_insert_nonfull(struct node_t * x, char * k) {
@@ -107,7 +127,7 @@ void btree_insert_nonfull(struct node_t * x, char * k) {
 				break;
 		}
 		//x->keys[i + 1] = k; // !
-		btree_node_copy_key( x, i+1, k );
+		btree_node_load_key(x, i + 1, k);
 		x->n++;
 		//
 	} else {
@@ -158,6 +178,116 @@ void btree_insert(struct node_t ** t, char * k) {
 	}
 }
 
+int btree_delete( struct node_t ** root, char * k )
+{
+	return btree_delete_detail( root, *root, k );
+}
+
+int btree_delete_detail(struct node_t ** root, struct node_t * x, char * k) {
+	// если ключ k находится в узле x и x является листом -- удаляем ключ k из x.
+	if (x->is_leaf) {
+		int i;
+		for (i = 0; i < x->n; i++) {
+			int cmp_res = strcmp(k, x->keys[i]);
+			if (cmp_res < 0)
+				break;
+			if (cmp_res == 0) {
+				// удаляем
+				if (x->keys[i] == NULL) {
+					fprintf( stderr, "[FATAL]: x->keys[i] == NULL.");
+					exit(1);
+				}
+				free(x->keys[i]);
+
+				int j;
+				for (j = i + 1; j < x->n; j++)
+					x->keys[j - 1] = x->keys[j];
+				x->n--;
+				return 0;
+				//break; // будем надеяться, что он такой один
+			}
+		}
+	} else {
+		int i;
+		int found_index = -1;
+		for (i = 0; i < x->n; i++) {
+			if (strcmp(k, x->keys[i]) == 0) {
+				found_index = i;
+				break;
+			}
+		}
+		if (found_index >= 0) {
+			fprintf(stderr, "[INFO]: btree_delete: ключ во внутреннем узле.\n");
+			struct node_t * y = x->pointers[found_index];
+			if (y->n >= MIN_CHILDREN) {
+				fprintf(stderr,
+						"[TODO]: btree_delete: узел слева достаточно большой.\n");
+				exit(1);
+			} else {
+				struct node_t * z = x->pointers[found_index + 1];
+				if (z->n >= MIN_CHILDREN) {
+					fprintf(stderr,
+							"[INFO]: btree_delete: узел справа достаточно большой. Удаляем в нём.\n");
+					char * k1 = btree_node_save_key(z, 0);
+					fprintf(stderr,"Удаление вида: %d %s\n", btree_delete_detail(root, z, k1), k1);
+					btree_node_load_key(x, found_index, k1);
+					return 1;
+				} else {
+					fprintf(stderr,
+							"[INFO]: btree_delete: ключи слева и справа можно объединить после удаления.\n");
+					// внести k в y
+					btree_node_load_key(y, y->n, k);
+					y->n++;
+
+					// внести все ключи и указатели z в y
+					int i;
+					for (i = 0; i < z->n; i++) {
+						// TODO: усложнить
+						char * kx = btree_node_save_key(z, i);
+						btree_node_load_key(y, y->n + i, kx);
+					}
+					for (i = 0; i <= z->n; i++) {
+						y->pointers[y->n + i] = z->pointers[i];
+					}
+					y->n += z->n;
+
+					// удалить k из x
+					for (i = found_index; i < x->n; i++) {
+						x->keys[i] = x->keys[i + 1];
+					}
+					// удалить указатель на z из x
+					for (i = found_index; i <= x->n; i++) {
+						x->pointers[i] = x->pointers[i + 1];
+					}
+					x->n--;
+					// освободить z
+					btree_node_free(z); //функция должна знать z.n, поэтому мы его не меняли
+
+					// если в x ничего не осталось, то y -- новый корень
+					if( x->n == 0 )
+					{
+						*root = y;
+						btree_node_free(x);
+					}
+
+					/*printf("До удаления");
+					btree_print( *root );*/
+					// удалить k из y
+					int delete_code = btree_delete_detail( root, y, k );
+					/*printf("После удаления");
+					btree_print( *root );*/
+					printf("Удаление вида: %d %s\n", delete_code, k);
+					return 2;
+				}
+			}
+		} else {
+			fprintf(stderr, "[TODO]: btree_delete: !is_leaf.\n");
+			exit(1);
+		}
+
+	}
+}
+
 int main(void) {
 	root = btree_create();
 
@@ -169,23 +299,28 @@ int main(void) {
 	}
 	int i;
 	/*char str[256];
-	for(i=0;i<100;i++)
-	{
-		sprintf( str, "%02d", i);
-		btree_insert(&root, str);
-	}*/
+	 for(i=0;i<100;i++)
+	 {
+	 sprintf( str, "%02d", i);
+	 btree_insert(&root, str);
+	 }*/
 	btree_insert(&root, "01_first");
-	/*btree_insert(&root, "02_second");
+	btree_insert(&root, "02_second");
 	btree_insert(&root, "03_third");
 	btree_insert(&root, "04_fourth");
 	btree_insert(&root, "05_fifth");
 	btree_insert(&root, "06_sixth");
 	btree_insert(&root, "07_seventh");
 	btree_insert(&root, "08_eighth");
-	btree_insert(&root, "09_nineth");
-	btree_insert(&root, "10_tenth");
-	btree_insert(&root, "11_eleventh");
-	btree_insert(&root, "12_twelfth");*/
+
+	btree_delete(&root, "04_fourth");
+	btree_delete(&root, "05_fifth");
+
+	//btree_delete(root, "04_fourth");
+	/*btree_insert(&root, "09_nineth");
+	 btree_insert(&root, "10_tenth");
+	 btree_insert(&root, "11_eleventh");
+	 btree_insert(&root, "12_twelfth");*/
 
 	btree_print(root);
 
